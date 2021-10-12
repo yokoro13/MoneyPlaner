94acbc3e56bb82fd72b736419d333e9525b3f2c5
yokoro13 <yokoro.kaito.yi3@is.naist.jp>
2021-10-12 15:13:19 +0900

    Update Entityにidを追加

--- app/src/main/java/com/yokoro/moneyplanner/domain/entity/actual/ActualIncome.kt
+++ app/src/main/java/com/yokoro/moneyplanner/domain/entity/actual/ActualIncome.kt
@@ -5,6 +5,7 @@ import com.yokoro.moneyplanner.domain.entity.shared.Expenditure
 import com.yokoro.moneyplanner.domain.entity.shared.Reason
 
 data class ActualIncome (
+    val id: Int, // FIXME domain層としてはもつべきでなさそう(data層のことを意識している)
     val date: Date,
     val reason: Reason,
     val value: Expenditure
